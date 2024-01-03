7e35382398a0bbbf8cc6aee263e9cd5dbf4102ec
Eredrim <Eredrim@users.noreply.github.com>
2024-01-03 19:27:38 +0100

    refactor: rewrite and fix LateLounge

--- m_LateLounge/src/main/java/net/slipcor/pvparena/modules/latelounge/LateLounge.java
+++ m_LateLounge/src/main/java/net/slipcor/pvparena/modules/latelounge/LateLounge.java
@@ -1,20 +1,23 @@
 package net.slipcor.pvparena.modules.latelounge;
 
 import net.slipcor.pvparena.arena.ArenaPlayer;
-import net.slipcor.pvparena.commands.AbstractArenaCommand;
-import net.slipcor.pvparena.commands.PAG_Join;
+import net.slipcor.pvparena.arena.PlayerStatus;
 import net.slipcor.pvparena.core.Config.CFG;
 import net.slipcor.pvparena.core.Language.MSG;
 import net.slipcor.pvparena.exceptions.GameplayException;
 import net.slipcor.pvparena.exceptions.GameplayExceptionNotice;
 import net.slipcor.pvparena.loadables.ArenaModule;
+import net.slipcor.pvparena.managers.WorkflowManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+
+import static java.util.Arrays.asList;
 
 public class LateLounge extends ArenaModule {
     public LateLounge() {
@@ -33,76 +36,73 @@ public class LateLounge extends ArenaModule {
         return PRIORITY;
     }
 
-    private final List<String> playerList = new ArrayList<>();
+    private final List<Player> playerList = new ArrayList<>();
 
     @Override
     public boolean handleJoin(Player player) throws GameplayExceptionNotice {
         int requiredPlayers = this.arena.getConfig().getInt(CFG.READY_MINPLAYERS);
 
-        if (this.playerList.contains(player.getName())) {
+        boolean isLoungeOpen = this.arena.getFighters().stream()
+                .anyMatch(ap -> asList(PlayerStatus.LOUNGE, PlayerStatus.READY).contains(ap.getStatus()));
+
+        if(isLoungeOpen) {
+            return false;
+        }
+
+        // Player already joined
+        if (this.playerList.contains(player)) {
             if (this.playerList.size() < requiredPlayers) {
-                int pos = this.playerList.indexOf(player.getName());
+                int pos = this.playerList.indexOf(player) + 1;
                 this.arena.msg(player, MSG.MODULE_LATELOUNGE_POSITION, String.valueOf(pos));
-                
                 throw new GameplayExceptionNotice(MSG.MODULE_LATELOUNGE_WAIT);
             }
         }
 
+        // not enough players
         if (requiredPlayers > this.playerList.size() + 1) {
-            // not enough players
-            this.playerList.add(player.getName());
-            final int pos = this.playerList.size();
-            for (final Player p : Bukkit.getOnlinePlayers()) {
-                if (!p.equals(player)) {
-                    try {
-                        this.arena.msg(p, MSG.MODULE_LATELOUNGE_ANNOUNCE, this.arena.getName(), player.getName());
-                    } catch (Exception ignored) {
+            this.playerList.add(player);
+            ArenaPlayer.fromPlayer(player).setQueuedArena(this.arena);
 
-                    }
-                }
-            }
-            this.arena.msg(player, MSG.MODULE_LATELOUNGE_POSITION, String.valueOf(pos));
+            Bukkit.getOnlinePlayers().stream()
+                    .filter(p -> !p.equals(player))
+                    .forEach(p -> {
+                        try {
+                            this.arena.msg(p, MSG.MODULE_LATELOUNGE_ANNOUNCE, this.arena.getName(), player.getName());
+                        } catch (Exception ignored) {}
+                    });
+            this.arena.msg(player, MSG.MODULE_LATELOUNGE_POSITION, String.valueOf(this.playerList.size()));
             throw new GameplayExceptionNotice(MSG.MODULE_LATELOUNGE_WAIT);
         }
 
         if (requiredPlayers == this.playerList.size() + 1) {
             // if this player is ok => enough players
-            this.playerList.add(player.getName());
+            this.playerList.add(player);
 
-            Set<String> removals = new HashSet<>();
+            Set<Player> readyList = new HashSet<>();
 
-            for (String s : this.playerList) {
-                Player p = Bukkit.getPlayerExact(s);
-
-                if (p != null) {
-                    for (ArenaModule mod : this.arena.getMods()) {
-                        if (!mod.getName().equals(this.getName())) {
-                            try {
-                                mod.checkJoin(p);
-                            } catch (GameplayException e) {
-                                this.arena.msg(p, MSG.MODULE_LATELOUNGE_REJOIN);
-                                removals.add(s);
-                                break;
-                            }
-                        }
+            this.playerList.stream().filter(Objects::nonNull).forEach(p -> {
+                this.arena.getMods().stream().filter(mod -> !mod.getName().equals(this.getName())).forEach(mod -> {
+                    try {
+                        mod.checkJoin(p);
+                        readyList.add(p);
+                    } catch (GameplayException e) {
+                        this.arena.msg(p, MSG.MODULE_LATELOUNGE_REJOIN);
                     }
-                } else {
-                    removals.add(s);
-                }
-            }
+                });
+            });
 
-            if (!removals.isEmpty()) {
-                this.playerList.removeAll(removals);
+            if (readyList.size() < this.playerList.size()) {
+                this.playerList.removeIf(p -> !readyList.contains(p));
                 throw new GameplayExceptionNotice(MSG.MODULE_LATELOUNGE_WAIT);
             } else {
                 // SUCCESS!
-                for (String s : this.playerList) {
-                    if (!s.equals(player.getName())) {
-                        Player p = Bukkit.getPlayerExact(s);
-                        AbstractArenaCommand command = new PAG_Join();
-                        command.commit(this.arena, p, new String[0]);
-                    }
-                }
+                this.playerList.stream()
+                        .filter(p -> !p.equals(player))
+                        .forEach(p -> {
+                            ArenaPlayer.fromPlayer(p).setQueuedArena(null);
+                            WorkflowManager.handleJoin(this.arena, p, new String[0]);
+                        });
+                this.playerList.clear();
             }
         }
         // enough, ignore and let something else handle the start!
@@ -110,15 +110,11 @@ public class LateLounge extends ArenaModule {
     }
 
     @Override
-    public boolean hasSpawn(final String name, final String teamName) {
-        return this.playerList.contains(name);
-    }
-
-    @Override
-    public boolean handleSpecialLeave(final ArenaPlayer arenaPlayer) {
-        if(this.playerList.contains(arenaPlayer.getName())) {
-            this.playerList.remove(arenaPlayer.getName());
+    public boolean handleQueuedLeave(final ArenaPlayer arenaPlayer) {
+        if(this.playerList.contains(arenaPlayer.getPlayer())) {
+            this.playerList.remove(arenaPlayer.getPlayer());
             this.arena.msg(arenaPlayer.getPlayer(), MSG.MODULE_LATELOUNGE_LEAVE, this.arena.getName());
+            arenaPlayer.setQueuedArena(null);
             return true;
         }
         return false;
