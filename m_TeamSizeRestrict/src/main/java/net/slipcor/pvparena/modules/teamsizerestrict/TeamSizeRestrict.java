b7acf084b3c4cc9e8d9847eab6e8ce2ea2af3576
Eredrim <Eredrim@users.noreply.github.com>
2024-01-04 13:45:45 +0100

    refactor: rewrite and improve TeamSizeRestrict

--- m_TeamSizeRestrict/src/main/java/net/slipcor/pvparena/modules/teamsizerestrict/TeamSizeRestrict.java
+++ m_TeamSizeRestrict/src/main/java/net/slipcor/pvparena/modules/teamsizerestrict/TeamSizeRestrict.java
@@ -1,47 +1,163 @@
 package net.slipcor.pvparena.modules.teamsizerestrict;
 
 
-import net.slipcor.pvparena.PVPArena;
+import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.arena.ArenaTeam;
-import net.slipcor.pvparena.commands.PAG_Leave;
+import net.slipcor.pvparena.commands.AbstractArenaCommand;
+import net.slipcor.pvparena.commands.CommandTree;
+import net.slipcor.pvparena.core.Config;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.Language.MSG;
+import net.slipcor.pvparena.core.RandomUtils;
+import net.slipcor.pvparena.exceptions.GameplayException;
 import net.slipcor.pvparena.loadables.ArenaModule;
-import org.bukkit.Bukkit;
+import net.slipcor.pvparena.managers.PermissionManager;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static net.slipcor.pvparena.config.Debugger.debug;
+
 public class TeamSizeRestrict extends ArenaModule {
+
+    private static final String CFG_PATH = "modules.teamsize";
+
     public TeamSizeRestrict() {
         super("TeamSizeRestrict");
     }
 
     @Override
     public String version() {
-        return getClass().getPackage().getImplementationVersion();
+        return this.getClass().getPackage().getImplementationVersion();
     }
 
     @Override
-    public void parseJoin(final Player player, final ArenaTeam team) {
-        try {
-            final Integer i = Integer.parseInt(arena.getConfig().getUnsafe("modules.teamsize." + team.getName()).toString());
-            if (team.getTeamMembers().size() > i) {
-                class RunLater implements Runnable {
+    public boolean checkCommand(final String s) {
+        return "!tsr".equalsIgnoreCase(s) || "teamsizerestrict".equalsIgnoreCase(s);
+    }
 
-                    @Override
-                    public void run() {
-                        arena.msg(player, ChatColor.RED + Language.parse(MSG.ERROR_JOIN_TEAM_FULL, team.getName()));
-                        new PAG_Leave().commit(arena, player, new String[0]);
-                    }
+    @Override
+    public List<String> getMain() {
+        return Collections.singletonList("teamsizerestrict");
+    }
 
-                }
+    @Override
+    public List<String> getShort() {
+        return Collections.singletonList("!tsr");
+    }
 
-                Bukkit.getScheduler().runTaskLater(PVPArena.getInstance(), new RunLater(), 1L);
+    @Override
+    public CommandTree<String> getSubs(final Arena arena) {
+        CommandTree<String> result = new CommandTree<>(null);
+        arena.getTeamNames().forEach(teamName -> result.define(new String[]{teamName}));
+        return result;
+    }
 
-            }
-        } catch (final Exception e) {
-            arena.getConfig().setManually("modules.teamsize." + team.getName(), -1);
-            arena.getConfig().save();
+    @Override
+    public void commitCommand(final CommandSender sender, final String[] args) {
+        // !tsr [team] [size]
+
+        if (!PermissionManager.hasAdminPerm(sender) && !PermissionManager.hasBuilderPerm(sender, this.arena)) {
+            this.arena.msg(sender, MSG.ERROR_NOPERM, Language.parse(MSG.ERROR_NOPERM_X_ADMIN));
+            return;
         }
+
+        if (!AbstractArenaCommand.argCountValid(sender, this.arena, args, new Integer[]{3})) {
+            return;
+        }
+
+        ArenaTeam matchedTeam = this.arena.getTeam(args[1]);
+        if(matchedTeam != null) {
+            String fullPath = String.format("%s.%s", CFG_PATH, matchedTeam.getName());
+            try {
+                this.arena.getConfig().setManually(fullPath, Integer.parseInt(args[2]));
+                this.arena.getConfig().save();
+                this.arena.msg(sender, MSG.SET_DONE, fullPath, args[2]);
+            } catch (NumberFormatException e) {
+                this.arena.msg(sender, MSG.ERROR_NOT_NUMERIC, args[2]);
+            }
+        } else {
+            this.arena.msg(sender, MSG.ERROR_TEAM_NOT_FOUND, args[1]);
+        }
+    }
+
+    @Override
+    public ArenaTeam choosePlayerTeam(Player player, ArenaTeam team, boolean canSwitch) throws GameplayException {
+        try {
+            int maxTeam = Integer.parseInt(this.arena.getConfig().getUnsafe(String.format("%s.%s", CFG_PATH, team.getName())).toString());
+            if (maxTeam > 0 && team.getTeamMembers().size() >= maxTeam) {
+                if(canSwitch) {
+                    ArenaTeam secondTryTeam = this.getRandomTeam();
+                    if(secondTryTeam == null) {
+                        this.arena.msg(player, String.format("%s%s", ChatColor.RED, Language.parse(MSG.ERROR_JOIN_ARENA_FULL)));
+                        throw new GameplayException(MSG.ERROR_JOIN_ARENA_FULL);
+                    } else {
+                        return secondTryTeam;
+                    }
+                } else {
+                    this.arena.msg(player, String.format("%s%s", ChatColor.RED, Language.parse(MSG.ERROR_JOIN_TEAM_FULL, team.getName())));
+                    throw new GameplayException(Language.parse(MSG.ERROR_JOIN_TEAM_FULL, team.getName()));
+                }
+            }
+        } catch (NumberFormatException | NullPointerException e) {
+            this.arena.getConfig().setManually(String.format("%s.%s", CFG_PATH, team.getName()), 0);
+            this.arena.getConfig().save();
+        }
+        return null;
+    }
+
+    @Override
+    public void configParse(YamlConfiguration config) {
+        this.initConfig();
+    }
+
+    @Override
+    public void initConfig() {
+        Config config = this.arena.getConfig();
+        this.arena.getTeamNames().forEach(teamName -> {
+            String fullPath = String.format("%s.%s", CFG_PATH, teamName);
+            if(!config.getYamlConfiguration().contains(fullPath)) {
+                config.setManually(fullPath, 0);
+            }
+        });
+        config.save();
+    }
+
+    private ArenaTeam getRandomTeam() {
+        debug(this.arena, "calculating free team for TeamSizeRestrict");
+
+        int maxTeamPlayersCfg = this.arena.getConfig().getInt(Config.CFG.READY_MAXTEAMPLAYERS);
+
+        // collect the available teams into a map and get count of "players missing" or "space available"
+        Map<ArenaTeam, Integer> availableTeamsWithMemberCount = new HashMap<>();
+
+        this.arena.getTeams().forEach(arenaTeam -> {
+                int maxTeamSize = maxTeamPlayersCfg;
+                try {
+                    maxTeamSize = Integer.parseInt(this.arena.getConfig().getUnsafe(String.format("%s.%s", CFG_PATH, arenaTeam.getName())).toString());
+                } catch (NumberFormatException ignored) {}
+
+                int maxPlayerPerTeam = (maxTeamSize == 0) ? 100 : maxTeamSize;
+
+                // don't map full teams
+                if(arenaTeam.getTeamMembers().size() < maxPlayerPerTeam) {
+                    availableTeamsWithMemberCount.put(arenaTeam, maxPlayerPerTeam - arenaTeam.getTeamMembers().size());
+                }
+        });
+
+        if(availableTeamsWithMemberCount.isEmpty()) {
+            // all teams are full
+            return null;
+        }
+
+        // pick a random team considering free spaces as a weight for computing randomness
+        return RandomUtils.getWeightedRandom(availableTeamsWithMemberCount, new Random());
     }
 }
